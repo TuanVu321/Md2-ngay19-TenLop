257675cac0f2a756676f5f78ff16fde2e98072de
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-04-15 15:25:11 +0700

    Add files via upload

---
+++ ten lop hoc/src/Class.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Class {
+    public static final String CLASS_REGEX = "^[CAP][0-9]{4}[GHIKLM]$";
+
+    public Class() {
+    }
+
+    public boolean validate(String regex) {
+        Pattern pattern = Pattern.compile(CLASS_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
---
+++ ten lop hoc/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Class lop = new Class();
+        System.out.println(lop.validate("C0318G"));
+        System.out.println(lop.validate("M0318G"));
+        System.out.println(lop.validate("P0323A"));
+        System.out.println(lop.validate("CG"));
+    }
+}
